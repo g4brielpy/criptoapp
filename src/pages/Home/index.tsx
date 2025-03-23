4c6119a6cb6e6978219c377293d5d6f329ea5075
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-23 09:18:19 -0300

    Adicinando icon de busca

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,19 @@
+import { BsSearch } from "react-icons/bs";
+
 export default function Home() {
   return (
-    <main>
-      <h1>pagina Home</h1>
+    <main className="container mx-auto">
+      <form action="#">
+        <input
+          type="text"
+          name="name-crypt"
+          placeholder="Digite o nome da Crypto"
+          required
+        />
+        <button type="submit">
+          <BsSearch size={"2rem"} color="#fff" />
+        </button>
+      </form>
     </main>
   );
 }
