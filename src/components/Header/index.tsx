a27bdea16b783c4ee01df388c560ddcad08d6c76
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-23 08:53:00 -0300

    Estilo header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import Logo from "../../assets/logo.svg";
 
 export function Header() {
   return (
-    <header>
+    <header className="container flex items-center justify-center h-32">
       <Link to={"/"}>
         <img src={Logo} alt="Logo CriptoMoeda" />
       </Link>
