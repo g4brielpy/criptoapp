729394b8b93d4b2fd3c9f82e039516f393c1b89a
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-24 06:50:32 -0300

    Formatação compacta

--- src/utils/formatedPrice.ts
+++ src/utils/formatedPrice.ts
@@ -2,3 +2,9 @@ export const formatedPrice = Intl.NumberFormat("en-US", {
   currency: "USD",
   style: "currency",
 });
+
+export const formatedPriceCompact = Intl.NumberFormat("en-US", {
+  currency: "USD",
+  style: "currency",
+  notation: "compact",
+});
