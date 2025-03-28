b4bb7e22df9c6ae4382169385b739914639be350
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-28 06:53:37 -0300

    Fazendo a requisição da cripto

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { api } from "../../api/coincap";
 
 export default function Detail() {
+  // id para fazer a requisição da cripto selecionada
   const { cripto } = useParams();
+  const [criptoFecth, SetCriptoFecth] = useState([]);
+
+  useEffect(() => {
+    async function getCoin() {
+      try {
+        const response = await api
+          .get(`/assets/${cripto}`)
+          .then((response) => response.data.data);
+        // Arrumar retorno da api
+        console.log(response);
+      } catch (e) {
+        console.log("Erro: " + e);
+      }
+    }
+
+    getCoin();
+  }, [cripto]);
 
   return (
     <main>
