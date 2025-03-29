1e32a212476ada6e2651d07065f08a607f9453ce
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-29 17:15:21 -0300

    Adicionando button para voltar

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,13 +1,15 @@
 import { useFetchCripto } from "../../hooks/useFetchCripto";
+import { useNavigate } from "react-router";
 import { useParams } from "react-router";
 
 import { Loading } from "./Loading";
 import { CryptoIcon } from "../../components/CryptoIcon";
+import { ButtonMore } from "../../components/ButtonMore";
 
 export default function Detail() {
   const { cripto } = useParams();
   const { criptoFetch, loading } = useFetchCripto(cripto);
-  console.log(criptoFetch);
+  const navegate = useNavigate();
 
   if (loading || !criptoFetch) {
     return <Loading />;
@@ -18,43 +20,46 @@ export default function Detail() {
       <h1 className="text-white text-center text-4xl font-extrabold mt-10 leading-[3.2rem]">
         {criptoFetch?.name} <br /> {criptoFetch?.symbol}
       </h1>
-      <section className="bg-neutral-950 text-white max-w-[420px] p-6 rounded-lg">
-        <ul className="space-y-3">
-          <li>
-            <CryptoIcon
-              name={criptoFetch?.name}
-              idCripto={criptoFetch?.id}
-              symbol={criptoFetch?.symbol}
-              className="w-16 h-auto object-cover hover:animate-spin"
-            />
-          </li>
-          <li>
-            <h2 className="font-bold text-2xl mb-6">
-              {criptoFetch?.name} | {criptoFetch?.symbol}
-            </h2>
-          </li>
-          <li>
-            <strong>Preço:</strong> {criptoFetch?.priceUsd}
-          </li>
-          <li>
-            <strong>Mercado:</strong> {criptoFetch?.marketCapUsd}
-          </li>
-          <li>
-            <strong>Volume:</strong> {criptoFetch?.volumeUsd24Hr}
-          </li>
-          <li>
-            <strong>mudanças 24h:</strong>{" "}
-            <span
-              className={
-                Number(criptoFetch?.changePercent24Hr) > 0
-                  ? "text-red-900 font-bold"
-                  : "text-green-900 font-bold"
-              }
-            >
-              {Number(criptoFetch?.changePercent24Hr).toFixed(2)}
-            </span>
-          </li>
-        </ul>
+      <section className="max-w-[420px] mx-auto my-6">
+        <div className="bg-neutral-950 text-white mb-10 p-6 rounded-lg">
+          <ul className="space-y-3">
+            <li>
+              <CryptoIcon
+                name={criptoFetch?.name}
+                idCripto={criptoFetch?.id}
+                symbol={criptoFetch?.symbol}
+                className="w-16 h-auto object-cover hover:animate-spin"
+              />
+            </li>
+            <li>
+              <h2 className="font-bold text-2xl mb-6">
+                {criptoFetch?.name} | {criptoFetch?.symbol}
+              </h2>
+            </li>
+            <li>
+              <strong>Preço:</strong> {criptoFetch?.priceUsd}
+            </li>
+            <li>
+              <strong>Mercado:</strong> {criptoFetch?.marketCapUsd}
+            </li>
+            <li>
+              <strong>Volume:</strong> {criptoFetch?.volumeUsd24Hr}
+            </li>
+            <li>
+              <strong>mudanças 24h:</strong>{" "}
+              <span
+                className={
+                  Number(criptoFetch?.changePercent24Hr) > 0
+                    ? "text-red-900 font-bold"
+                    : "text-green-600 font-bold"
+                }
+              >
+                {Number(criptoFetch?.changePercent24Hr).toFixed(2)}
+              </span>
+            </li>
+          </ul>
+        </div>
+        <ButtonMore onClick={() => navegate("/")}>Voltar</ButtonMore>
       </section>
     </main>
   );
