83632e7758c296518d2767d81fbee1a9ebfbfbc4
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-05-20 21:09:53 -0300

    Atualizando api para v3

--- src/api/coincap.ts
+++ src/api/coincap.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "https://api.coincap.io/v2/",
+  baseURL: "https://rest.coincap.io/v3/",
+  params: {
+    apiKey: "bb2d1c81244334f1507d4bd5a0cc3a5d972fcdbd5086a5b1e6365e320833021f",
+  },
 });
